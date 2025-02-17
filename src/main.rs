da037c15e87324bd47891fa54c7df02531b8a2d5
Emil Lindgren <limelgren@gmail.com>
2025-02-17 21:34:47 +0100

    Create data layout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+struct Game<'a> {
+    board: &'a Board,
+    player1: &'a Player,
+    player2: &'a Player,
+}
+
+struct Player {
+    points: u16,
+    inventory: Vec<Tile>,
+}
+
+struct Board {
+    tiles: [Tile; 225],
+}
+
+impl Board {
+    fn new_board() -> Self {
+        let tiles = [Tile {
+            char: ' ',
+            points: 1,
+        }; 225];
+        Board { tiles }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Tile {
+    char: char,
+    points: u8,
+}
+
 fn main() {
-    println!("Hello, world 2!");
+    let board = Board::new_board();
+    let player1 = Player {
+        points: 0,
+        inventory: Vec::new(),
+    };
+    let player2 = Player {
+        points: 0,
+        inventory: Vec::new(),
+    };
+    let game = Game {
+        board: &board,
+        player1: &player1,
+        player2: &player2,
+    };
 }
